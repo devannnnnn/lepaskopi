70201198f0768d1f1f02c60ff279886aef597fcf
devannnnnn <devanoo161607@gmail.com>
2024-08-16 21:07:10 +0700

    abis nambahin modal detail

--- js/script.js
+++ js/script.js
@@ -2,25 +2,66 @@
 const navbarNav = document.querySelector(".navbar-nav");
 // ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = () => {
-  navbarNav.classList.toggle("active");  
+  navbarNav.classList.toggle("active");
 };
 
-// toggle class active untuk search form 
-  const searchform = document.querySelector('.search-form');
-  const searchbox = document.querySelector('#search-box');
+// toggle class active untuk search form
+const searchform = document.querySelector(".search-form");
+const searchbox = document.querySelector("#search-box");
 
-  document.querySelector("#search-button").onclick = (e) => {
-    e.preventDefault(); // Mencegah aksi default
-    searchform.classList.toggle("active");
-    searchbox.focus();
+document.querySelector("#search-button").onclick = (e) => {
+  e.preventDefault(); // Mencegah aksi default
+  searchform.classList.toggle("active");
+  searchbox.focus();
 };
 
-// klik diluar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector('#hamburger-menu');
+// toggle class active untuk shopping cart
+const shoppingcart = document.querySelector(".shopping-cart");
 
-document.addEventListener('click', function(e){
-  if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove('active'); 
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingcart.classList.toggle("active");
+  e.preventDefault();
+};
+
+// klik diluar element
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
+
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchform.contains(e.target)) {
+    searchform.classList.remove("active");
+  }
+  if (!sc.contains(e.target) && !shoppingcart.contains(e.target)) {
+    shoppingcart.classList.remove("active");
   }
 });
 
+// modal box
+const itemdetailmodal = document.querySelector("#item-detail-modal");
+const itemdetailbutton = document.querySelectorAll(".item-detail-button");
+
+itemdetailbutton.forEach(button => {
+  button.onclick = (e) => {
+    e.preventDefault();
+    itemdetailmodal.style.display = "flex";
+  };
+});
+
+// klik tombol close modal
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemdetailmodal.style.display = "none";
+  e.preventDefault();
+};
+
+// klik diluar modal 
+;
+window.onclick = (e) => {
+  if (e.target === itemdetailmodal) {
+    itemdetailmodal.style.display = 'none';
+  }
+}
